63a88e1df1fe765e8ae6c74279ba0f2cea7ad331
Tomaz <tomaz.perez@outlook.com>
2023-10-23 21:04:47 +0200

    Added a putmapping with validations. sends BAD_REQUEST if libraryEventId is null or if the libraryEventType is not UPDATE

--- src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
+++ src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
@@ -1,6 +1,7 @@
 package com.example.libraryeventsproducer.controller;
 
 import com.example.libraryeventsproducer.domain.LibraryEvent;
+import com.example.libraryeventsproducer.domain.LibraryEventType;
 import com.example.libraryeventsproducer.producer.LibraryEventsProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
@@ -8,11 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 
 @RestController
 @Slf4j
@@ -25,7 +25,7 @@ public class LibraryEventsController {
     }
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
 
         log.info("libraryEvent : {} ", libraryEvent);
 
@@ -35,5 +35,31 @@ public class LibraryEventsController {
         log.info("After sending libraryEvent: ");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    @PutMapping("/v1/libraryevent")
+    public ResponseEntity<?> updateLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        log.info("libraryEvent : {} ", libraryEvent);
+
+        ResponseEntity<String> BAD_REQUEST = validateLibraryEvent(libraryEvent);
+        if (BAD_REQUEST != null) return BAD_REQUEST;
+
+        libraryEventsProducer.sendLibraryEvent3(libraryEvent);
+
+        log.info("After sending libraryEvent: ");
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+    private static ResponseEntity<String> validateLibraryEvent(LibraryEvent libraryEvent) {
+        if (libraryEvent.libraryEventId() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the libraryEventId");
+        }
+
+        if (!libraryEvent.libraryEventType().equals(LibraryEventType.UPDATE)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only UPDATE event are supported");
+        }
+        return null;
     }
 }
